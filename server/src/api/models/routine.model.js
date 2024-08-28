70b9112aa3e664c54c47975b991c159878ca8804
Miguel Martinez <mgoni83@hotmail.com>
2024-08-28 11:28:50 +0200

    add routine_model

--- server/src/api/models/routine.model.js
+++ server/src/api/models/routine.model.js
@@ -2,6 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    name: { type: String, require: true },
+    category: { type: String, require: true, enum: ["face", "body", "mouth", "hair", "hands", "feet"] },
+    applyschedule: { type: String, require: true, enum: ["morning", "afternoon", "night", "other"] },
+    repeat: [{ type: String, require: true }],
+    products: [{
+        id: { type: Schema.Types.ObjectId, ref: "product" }
+    }],
+    usesteps: { type: String },
+    description: { type: String }
+
+
 }, {
     collection: "routine",
     timestamps: true
