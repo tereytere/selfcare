3ddca86cdd36b8e2e7046b1e90f7ab584e16a9d2
Laura Laguna <lauragl@gmail.com>
2024-08-28 11:48:21 +0200

    add review model

--- server/src/api/models/review.model.js
+++ server/src/api/models/review.model.js
@@ -2,6 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    title: { type: String, require: true },
+    description: { type: String, require: true },
+    date: { type: Date, default: Date.now },
+    status: {
+        type: String, enum: [
+            "show", "hidden"
+        ]
+    },
+    stars: {
+        type: Number, require: true, enum: [
+            1,
+            2,
+            3,
+            4,
+            5,
+        ],
+    },
 }, {
     collection: "review",
     timestamps: true
