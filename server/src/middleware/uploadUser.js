3d6b216cfb97022d7be37f67e56065ab36bb72e7
Chiara Magnani <chiara2mf@gmail.com>
2024-08-29 17:44:17 +0200

    fixed errors with cloudinary

--- server/src/middleware/uploadUser.js
+++ server/src/middleware/uploadUser.js
@@ -5,7 +5,7 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: "users",
+        folder: "selfcareuser",
         allowedFormats: ["jpg", "png", "jpeg"]
     }
 });
