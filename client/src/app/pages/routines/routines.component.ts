6655df4d6028abec8cff0978324c2d0d6582901a
Teresa <teresa.ambroa@hotmail.com>
2024-08-30 15:42:05 +0200

    pages components template created

---
+++ client/src/app/pages/routines/routines.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-routines',
+  standalone: true,
+  imports: [],
+  templateUrl: './routines.component.html',
+  styleUrl: './routines.component.css'
+})
+export class RoutinesComponent {
+
+}
