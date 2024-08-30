fc128ab512c746050ae25ba8c5b2f35d8a4e797a
Nachosam <ignacio.sampedroolivera@gmail.com>
2024-08-30 18:24:22 +0200

    logotipos añadidos

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { RoutinesComponent } from './pages/routines/routines.component';
+import { CardReviewAllComponent } from "./components/card-review-all/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, RoutinesComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, RoutinesComponent, CardReviewAllComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/src/app/components/review-form/review-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './review-form.component.html',
+  styleUrl: './review-form.component.css'
+})
+export class ReviewFormComponent {
+
+}
