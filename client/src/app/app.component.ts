3848f9fc9b2facbf8c859fecc629653cc788e2bd
Miguel Martinez <mgoni83@hotmail.com>
2024-08-30 18:07:42 +0200

    Commit de la caca

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,14 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { InputLabelComponent } from './components/input-label/input-label.component';
-import { DetailProductComponent } from './pages/detail-product/detail-product.component';
 import { DetailRoutineComponent } from "./pages/detail-routine/detail-routine.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, InputLabelComponent, DetailProductComponent],
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DetailRoutineComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, InputLabelComponent, DetailRoutineComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
