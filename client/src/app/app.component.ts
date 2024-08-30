1714e6fa347e1160b124b10405178ca3ea8eccfa
Laura Laguna <lauragl@gmail.com>
2024-08-30 18:38:55 +0200

    form producto

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,12 +5,12 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { InputLabelComponent } from './components/input-label/input-label.component';
 import { DetailProductComponent } from './pages/detail-product/detail-product.component';
 import { DetailRoutineComponent } from "./pages/detail-routine/detail-routine.component";
-import { ProductFormComponent } from './pages/product-form/product-form.component';
+/* import { ProductFormComponent } from './pages/product-form/product-form.component'; */
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, InputLabelComponent, DetailProductComponent, ProductFormComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, InputLabelComponent, DetailProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
