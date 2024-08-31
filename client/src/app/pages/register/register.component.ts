52d225f93dbd78042b3f9d27d07847b0b81bc48f
Miguel Martinez <mgoni83@hotmail.com>
2024-08-31 19:21:30 +0200

    formularios varios

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,20 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { CITY_COORDINATES } from '../../data/city-coordinates';
 
 
 @Component({
   selector: 'register',
   standalone: true,
-  imports: [ButtonModule, FloatLabelModule, FormsModule, ReactiveFormsModule,],
+  imports: [ButtonModule, FloatLabelModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  cities: string[] = [];
 
 
   private userService = inject(UserService);
@@ -30,6 +32,14 @@ export class RegisterComponent {
   })
   errores: { message: string }[] = [];
 
+  ngOnInit(): void {
+    this.loadCities();
+  }
+
+  loadCities(): void {
+    this.cities = Object.keys(CITY_COORDINATES);
+  }
+
   onUpload(event: any) {
     const input = event.target as HTMLInputElement;
     if (input && input.files && input.files.length > 0) {
@@ -41,10 +51,7 @@ export class RegisterComponent {
 
   async onSubmit() {
     try {
-      console.log(this.formulario.value);
-
       const response = await this.userService.register(this.formulario.value);
-      console.log(response.message)
       this.router.navigate(['/login']);
     } catch (error) {
       this.errores = [{ message: 'An error occurred' }];
