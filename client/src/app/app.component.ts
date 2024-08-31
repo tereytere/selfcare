306ea3bfa2907d9d11ac627879bdf7989993a03a
Nachosam <ignacio.sampedroolivera@gmail.com>
2024-08-31 13:36:44 +0200

    merge nacho

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,15 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { RoutinesComponent } from './pages/routines/routines.component';
-import { CardReviewAllComponent } from "./components/card-review-all/card.component";
-import { ReviewFormComponent } from './components/review-form/review-form.component';
 
 
 @Component({
   selector: 'root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, RoutinesComponent, CardReviewAllComponent, ReviewFormComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
