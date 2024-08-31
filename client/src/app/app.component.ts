b618d8030b56295da9ebf4cf220e46fbd21a1e49
Nachosam <ignacio.sampedroolivera@gmail.com>
2024-08-31 19:36:37 +0200

    working carousel

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 
 @Component({
   selector: 'root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product.interface';
+
+@Component({
+  selector: 'carousel-component',
+  standalone: true,
+  imports: [CarouselModule, ButtonModule, TagModule],
+  providers: [ProductService],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+
+  products: Product[] | undefined;
+
+  responsiveOptions: any[] | undefined;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.getAll(1, 5).then((products) => {
+      this.products = products;
+    });
+
+
+  }
+
+}
