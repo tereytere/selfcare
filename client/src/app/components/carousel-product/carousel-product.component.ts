0c165f35ac8b9c089654483139e548d4ae6cac3c
Miguel Martinez <mgoni83@hotmail.com>
2024-09-02 20:49:30 +0200

    Estilos varios, e implementar en la home

---
+++ client/src/app/components/carousel-product/carousel-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'carousel-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel-product.component.html',
+  styleUrl: './carousel-product.component.css'
+})
+export class CarouselProductComponent {
+
+}
