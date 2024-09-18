fb5fdb0e7abd9fe59411a27e05e2cae86283c4d8
Teresa <teresa.ambroa@hotmail.com>
2024-09-18 17:07:06 +0200

    trying to fix footer

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'footer-component',
   standalone: true,
-  imports: [],
+  imports: [FloatLabelModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
 
